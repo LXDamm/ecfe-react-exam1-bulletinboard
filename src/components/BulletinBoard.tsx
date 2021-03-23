301c55318f0e0896671f48af9cc9c7ff25ac94ec
Lexx Damm <lexx.damm@gmail.com>
2021-03-23 18:27:01 +0100

    started answering questions

--- src/components/BulletinBoard.tsx
+++ src/components/BulletinBoard.tsx
@@ -9,19 +9,19 @@ function BulletinBoard() {
     const questions = [
         {
             question: 'Vad är tre fördelar med att använda React?',
-            answer: 'Det är snabbt att komma igång med. Det är populärt. Det är snabbt.'
+            answer: '1. Det är snabbt att komma igång med. 2. Det används av Facebook och är ett av dom populäraste frameworksen och har därför mycket dokumentation och guider på nätet. 3. Det är snabbt.'
         },
         {
             question: 'Vad är tre nackdelar med att använda React?',
-            answer: 'Det är gjort av Facebook. Det är stort'
+            answer: '1. Det är gjort av Facebook. 2. Det tar mer resurser än att enbart göra vanilla JavaScript/TypeScript och HTML.'
         },
         {
             question: 'Vad är tre skillnader mellan React och Angular?',
-            answer: ''
+            answer: '1. Angular har fler funktioner inbyggt i frameworket som React saknar, vilket gör att en både har större flexibeiltet med React, men också att en måste använda ytterligare biblotek för att få likvärdiga funktioner som Angular.'
         },
         {
             question: 'Vad är tre bibliotek man kan använda med React?',
-            answer: 'Redux. Bootstrap'
+            answer: '1. Redux. 2. Bootstrap'
         }
     ];
     const renderQuestions = (questions: Array<Question>) => {
